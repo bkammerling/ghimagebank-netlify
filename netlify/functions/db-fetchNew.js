1cc3fb8baa8abb43563c94d8baeb27d960ea2aa0
Ben Kammerling <bkammerling@googlemail.com>
2022-04-15 16:51:02 +0100

    firebase environment variable escaping issue fix

--- netlify/functions/db-fetchNew.js
+++ netlify/functions/db-fetchNew.js
@@ -7,7 +7,7 @@ if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       "projectId": process.env.FIREBASE_PROJECT_ID,
-      "private_key": process.env.FIREBASE_PRIVATE_KEY,
+      "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
       "client_email": process.env.FIREBASE_CLIENT_EMAIL,
     }),
     databaseURL: "https://good-humans-timesheet.firebaseio.com"
