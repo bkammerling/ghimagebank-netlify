e9fc71ad008cf56091b3f2d5e9570108656a7aad
Ben Kammerling <bkammerling@googlemail.com>
2025-04-30 08:17:12 +0100

    Latest db update for JESB

--- update-field.mongodb.js
+++ update-field.mongodb.js
@@ -7,8 +7,8 @@ use('image_bank');
 
 const filter = {
   $and: [
-    { brand: { $exists: true } },
-    { title: { $regex: '^JE.*'}}
+    { brand: { $exists: false } },
+    { title: { $regex: '^JESB.*'}}
   ]
 }
 
@@ -19,8 +19,6 @@ console.log(db.getCollection('images').find(filter).count());
 // log the first 10 documents with this filter
 db.getCollection('images').find(filter).limit(10).forEach(printjson);
 
-//db.getCollection('images').find(filter);
-
-//db.getCollection('images').updateMany(filter, { $set: { brand: "monsterstrat" }});
+// db.getCollection('images').updateMany(filter, { $set: { brand: "jewson" }});
 
 //monsterstrat
